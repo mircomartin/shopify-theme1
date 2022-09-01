1cc92d2721ff62acab1bd923b6f14571e757c21b
Mirco Martin <mirco.martin@ecloudsolutions.com>
2022-09-01 20:31:16 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*.liquid",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
